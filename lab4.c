87a925a72c8254a635a9184b8077120f45116e2c
Hanayasser <110638009+Hanayasser@users.noreply.github.com>
2022-08-08 02:23:22 +0200

    Add files via upload

---
+++ lab4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(void){
+
+int number1,number2;
+
+printf("Enter number1\n");
+
+scanf("%d",&number1);
+
+printf("Enter number2\n");
+
+scanf("%d",&number2);
+int sum;
+
+sum=number1+number2;
+
+int mul;
+
+mul=number1*number2;
+
+printf("the value of sum is:%d\n",sum);
+
+printf("the value of mul is:%d\n",mul);
+
+}
